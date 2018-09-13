4609fb13730cf8a473c1284074a6f44d882b0001
ke.xiong <ke.xiong>
2018-09-13 13:58:55 +0800

    jstack..

---
+++ src/test/java/guava/OrderingTest.java
@@ -0,0 +1,20 @@
+package guava;
+
+import com.google.common.collect.Ordering;
+
+/**
+ * Created by 1234qwer on 2018/8/31.
+ */
+public class OrderingTest {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(Ordering.natural().min(1,2));
+
+
+    }
+
+
+}
